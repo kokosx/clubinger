eadfc22845907167cd840381fffe971770bf903e
kokosx <agabart11@gmail.com>
2023-12-29 11:33:52 +0100

    feat(auth): added logo to auth pages

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -2,6 +2,7 @@ import type { ReactNode } from "react";
 
 import { redirect } from "next/navigation";
 import { getSession } from "@/lib/auth/utils";
+import Image from "next/image";
 
 type Props = {
   children: ReactNode;
@@ -21,20 +22,28 @@ const AuthLayout = async ({ children }: Props) => {
         <div className="flex h-full items-center bg-gray-900 bg-opacity-40 px-20">
           <div>
             <h2 className="text-2xl font-bold text-white sm:text-3xl">
-              Meraki UI
+              KultKlub
             </h2>
 
             <p className="mt-3 max-w-xl text-gray-300">
-              Lorem ipsum dolor sit, amet consectetur adipisicing elit. In autem
-              ipsa, nulla laboriosam dolores, repellendus perferendis libero
-              suscipit nam temporibus molestiae
+              Dołącz do klubów odpowiadających twoim zainteresowaniom
             </p>
           </div>
         </div>
       </div>
 
-      <div className="mx-auto flex w-full max-w-md items-center px-6 lg:w-2/6">
+      <div className="mx-auto flex h-screen w-full max-w-md items-center px-6 lg:w-2/6">
         <div className="flex-1">
+          <div className="flex w-full flex-col items-center justify-center gap-y-2">
+            <Image
+              src="/logo.png"
+              alt="logo"
+              height={100}
+              width={100}
+              className="rounded-lg"
+            />
+            <h2 className="text-4xl font-semibold">Witaj!</h2>
+          </div>
           <div className="mt-8">{children}</div>
         </div>
       </div>

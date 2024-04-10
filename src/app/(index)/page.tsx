1c6b23a7a42be28ff3d29f29f4c842bcb7780afd
kokosx <agabart11@gmail.com>
2024-04-10 21:33:47 +0200

    feat: added index page

--- src/app/(index)/page.tsx
+++ src/app/(index)/page.tsx
@@ -1,7 +1,21 @@
-import React from "react";
+import Link from "next/link";
+import { cn } from "@/lib/utils";
+import { buttonVariants } from "@/components/ui/button";
 
 const page = () => {
-  return <div>Welcome page</div>;
+  return (
+    <div className="flex flex-col items-center gap-y-1">
+      <h1 className="mt-[15%] text-center text-5xl font-bold md:text-8xl">
+        KultKlub
+      </h1>
+      <p className="text-center text-xl md:text-3xl">
+        Odkryj nowe kluby i znajdź ludzi z podobnymi upodobaniami
+      </p>
+      <Link href="/sign-up" className={cn(buttonVariants())}>
+        Zacznij teraz
+      </Link>
+    </div>
+  );
 };
 
 export default page;
